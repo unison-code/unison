d5bdb23c8a105e5ab710d637dfd34cbf9b20c7f1
Roberto Castaneda Lozano <rcas@sics.se>
2016-11-25 10:55:05 +0100

    Refine callee-saved temps analysis by checking the temp preassignments

--- src/unison/src/Unison/Analysis/CallingConvention.hs
+++ src/unison/src/Unison/Analysis/CallingConvention.hs
@@ -14,24 +14,33 @@ This file is part of Unison, see http://unison-code.github.io
 -}
 module Unison.Analysis.CallingConvention (calleeSavedTemps) where
 
+import Data.List
 import qualified Data.Set as S
 
 import Unison.Base
 import Unison.Util
 import Unison.Predicates
+import Unison.Constructors
 import qualified Unison.Graphs.SG as SG
 
 -- | Gives a set of callee-saved temporaries
-calleeSavedTemps f @ Function {fCode = code} =
-  let fCode  = flatten code
-      sg     = SG.fromFunction Nothing f
-      sp     = SG.sameTempPartitions sg
-      csp    = filter (isCalleeSavedPartition fCode) sp
+calleeSavedTemps csf ovf f @ Function {fCode = code} =
+  let fCode = flatten code
+      sg    = SG.fromFunction Nothing f
+      sp    = SG.sameTempPartitions sg
+      csr   = map (mkRegister . mkTargetRegister) csf
+      csp   = filter (isCalleeSavedPartition ovf csr fCode) sp
   in S.fromList $ concat csp
 
-isCalleeSavedPartition code = all (isOnlyRefByBoundaries code)
+isCalleeSavedPartition ovf csr code =
+  all (\t -> isOnlyRefByBoundaries code t &&
+             isCalleeSavedOverlap ovf csr code t)
 
 isOnlyRefByBoundaries code t =
     let usrs  = filter (isUser t) code
         dfrs  = filter (isDefiner t) code
     in all isIn dfrs && all isOut usrs
+
+isCalleeSavedOverlap ovf csr code t =
+  let rs = nub [r | (_, t', r) <- preAssignments [mkDummyBlock code], t == t']
+  in all (\r -> any (\r' -> ovf r r') csr) rs
