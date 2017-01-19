a1593aa3b85ab9ee15fcfc05df8f8e07914346e3
Roberto Castaneda Lozano <rcas@sics.se>
2017-01-19 14:04:36 +0100

    Create portfolio solver that runs gecode-solver and minizinc-solver with chuffed in parallel, returning the result of the fastest one

---
+++ src/solvers/multi_backend/portfolio.hs
@@ -0,0 +1,76 @@
+#!/usr/bin/env runhaskell
+
+{-|
+Copyright   :  Copyright (c) 2016, SICS Swedish ICT AB
+License     :  BSD3 (see the LICENSE file)
+Maintainer  :  rcas@sics.se
+
+Runs 'gecode-solver' and 'minizinc-solver' (with 'chuffed') in parallel,
+returning the result of the fastest solver.
+
+-}
+{-
+Main authors:
+  Roberto Castaneda Lozano <rcas@sics.se>
+
+This file is part of Unison, see http://unison-code.github.io
+-}
+
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
+
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Monad
+import System.Console.CmdArgs
+import System.Process
+import System.Process.Internals
+import System.Directory
+import Data.List.Split
+
+data Portfolio =
+  Portfolio {inFile  :: FilePath,
+             outFile :: FilePath}
+  deriving (Data, Typeable, Show)
+
+portfolioArgs = cmdArgsMode $ Portfolio
+    {
+     inFile  = "" &= argPos 1 &= typFile,
+     outFile = "" &= name "o" &= help "Output file name" &= typFile
+    }
+
+gecodeFile outJsonFile = outJsonFile ++ ".gecode"
+chuffedFile outJsonFile = outJsonFile ++ ".chuffed"
+
+runGecode extJson outJsonFile =
+  do callProcess "gecode-solver"
+       ["-o", outJsonFile, "--verbose", extJson]
+     return outJsonFile
+
+runChuffed extJson outJsonFile =
+  do callProcess "minizinc-solver"
+       ["--topdown", "--chuffed", "--free", "-o", outJsonFile, extJson]
+     return outJsonFile
+
+main =
+    do Portfolio{..} <- cmdArgsRun portfolioArgs
+       let gecodeOutFile  = outFile ++ ".gecode"
+           chuffedOutFile = outFile ++ ".chuffed"
+       result <- race
+                 (runGecode inFile gecodeOutFile)
+                 (runChuffed inFile chuffedOutFile)
+       -- FIXME: this is too brutal (and platform-specific), find out instead
+       -- why the process spawned by 'minizinc-solver' are not killed
+       createProcess
+         (proc "killall" ["sicstus", "mzn2fzn", "fzn-chuffed"])
+         {std_out = CreatePipe, std_err = CreatePipe}
+       let finalOutFile = case result of
+                           Left  outFile1 -> outFile1
+                           Right outFile2 -> outFile2
+       renameFile finalOutFile outFile
+       removeIfExists gecodeOutFile
+       removeIfExists chuffedOutFile
+       return ()
+
+removeIfExists file =
+  do fileExists <- doesFileExist file
+     when fileExists (removeFile file)
