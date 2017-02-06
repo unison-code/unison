15aeb3bb18b81d0469832caba059d19930aaad24
Roberto Castaneda Lozano <rcas@sics.se>
2017-02-06 11:14:25 +0100

    Fix trivial bug in CI script

--- .travis/download-build-gecode.sh
+++ .travis/download-build-gecode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./.accept-svn-certificate anonymous robcasloz@gmail.com https://svn.gecode.org/svn/gecode/trunk
+./.accept-svn-certificate.sh anonymous robcasloz@gmail.com https://svn.gecode.org/svn/gecode/trunk
 svn --non-interactive --username anonymous --password robcasloz@gmail.com checkout -r15375 https://svn.gecode.org/svn/gecode/trunk gecode-trunk
 cd gecode-trunk
 ./configure --disable-examples --disable-float --disable-flatzinc
