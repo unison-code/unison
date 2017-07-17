2dc04a930d6a01ed4ddfc17abd2b347120b6eeef
Roberto Castaneda Lozano <rcas@sics.se>
2017-07-17 16:45:39 +0200

    Update Gecode version

--- .travis/download-build-gecode.sh
+++ .travis/download-build-gecode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./.travis/accept-svn-certificate.sh anonymous robcasloz@gmail.com https://svn.gecode.org/svn/gecode/trunk
-svn --non-interactive --username anonymous --password robcasloz@gmail.com checkout -r15626 https://svn.gecode.org/svn/gecode/trunk gecode-trunk
+svn --non-interactive --username anonymous --password robcasloz@gmail.com checkout -r16106 https://svn.gecode.org/svn/gecode/trunk gecode-trunk
 cd gecode-trunk
 ./configure --disable-examples --disable-float --disable-flatzinc
 make
