fd4e614d9f79af8e17b80cc1a178b1a8015e78a2
Roberto Castaneda Lozano <rcas@sics.se>
2018-03-08 14:21:33 +0100

    Use original name of Gecode source directory

--- .travis/download-build-gecode.sh
+++ .travis/download-build-gecode.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 wget https://github.com/Gecode/gecode/archive/release-6.0.0.tar.gz
 tar -xvzf release-6.0.0.tar.gz
-mv gecode-release-6.0.0 gecode-build
-cd gecode-build && ./configure --disable-examples --disable-float --disable-flatzinc && make && cd -
+cd gecode-release-6.0.0 && ./configure --disable-examples --disable-float --disable-flatzinc && make && cd -
