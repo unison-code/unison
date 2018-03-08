1b50109a6f624a085df0e4e146813e228030d35f
Roberto Castaneda Lozano <rcas@sics.se>
2018-03-08 14:03:05 +0100

    Configure and build Gecode in one line in CI script

--- .travis/download-build-gecode.sh
+++ .travis/download-build-gecode.sh
@@ -2,7 +2,4 @@
 wget https://github.com/Gecode/gecode/archive/release-6.0.0.tar.gz
 tar -xvzf release-6.0.0.tar.gz
 mv gecode-release-6.0.0 gecode-build
-cd gecode-build
-./configure --disable-examples --disable-float --disable-flatzinc
-make
-cd -
+cd gecode-build && ./configure --disable-examples --disable-float --disable-flatzinc && make && cd -
