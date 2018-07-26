92e6bb8121f42486457f09dd31cf6e142ac97b3d
Roberto Castaneda Lozano <rcas@sics.se>
2018-07-26 14:37:08 +0200

    Extend 'maxTempId' function for bundles

--- src/unison/src/MachineIR/Util.hs
+++ src/unison/src/MachineIR/Util.hs
@@ -55,7 +55,9 @@ flattenMachineBlock MachineBlock {mbId = id, mbInstructions = is} =
 newMachineBlockId mf = maxZ (map mbId (mfBlocks mf)) + 1
 
 newMachineTempId mf = maxZ [maxTempId mi | mi <- flattenMachineFunction mf] + 1
-maxTempId mi = maxZ [id | MachineTemp {mtId = id} <- msOperands mi]
+maxTempId mi @ MachineSingle {} =
+  maxZ [id | MachineTemp {mtId = id} <- msOperands mi]
+maxTempId MachineBundle {mbInstrs = mis} = maxZ (map maxTempId mis)
 
 maxZ = maybeMax 0
 
