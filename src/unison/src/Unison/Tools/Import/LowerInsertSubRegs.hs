1f52ef3e89d1cff4c75d82f3464bf783751483a5
Mats Carlsson <matsc@sics.se>
2018-08-27 16:36:48 +0200

    Close(?) LowerInsertSubRegs

--- src/unison/src/Unison/Tools/Import/LowerInsertSubRegs.hs
+++ src/unison/src/Unison/Tools/Import/LowerInsertSubRegs.hs
@@ -26,32 +26,17 @@ lowerInsertSubRegs mf target =
   in mf'
 
 lowerInsertInstrSubRegs stf tid2rc (accIs, id) (mi @
-  MachineSingle {msOperands = [d @ MachineTemp {mtId = did},
-                               s1,
-                               s2 @ MachineTemp {},
-                               sr]}
-  : is)
+  MachineSingle {msOperands = [MachineTemp {mtId = did}, _, _, sr]} : is)
   | isMachineInsertSubReg mi =
-  let subreg     = toSubRegIndex sr
-      (id', mis) =
-        case stf (tid2rc M.! did) subreg of
-          [LowSubRegIndex] ->
-            let t  = mkSimpleMachineTemp id
-                hi = mi {msOpcode = mkMachineVirtualOpc HIGH,
-                         msOperands = [t, s1]}
-                co = mi {msOpcode = mkMachineVirtualOpc COMBINE,
-                         msOperands = [d, s2, t]}
-            in (id + 1, [hi, co])
-          [HighSubRegIndex] ->
-            let t  = mkSimpleMachineTemp id
-                lo = mi {msOpcode = mkMachineVirtualOpc LOW,
-                         msOperands = [t, s1]}
-                co = mi {msOpcode = mkMachineVirtualOpc COMBINE,
-                         msOperands = [d, t, s2]}
-            in (id + 1, [lo, co])
-          [CopySubRegIndex] -> (id, [mi {msOpcode = mkMachineVirtualOpc COPY,
-                                       msOperands = [d, s2]}])
+  let subreg                    = toSubRegIndex sr
+      subops                    = stf (tid2rc M.! did) subreg
+      (id', mis)                = lowerInsertInstrSubRegs' subops id mi
   in lowerInsertInstrSubRegs stf tid2rc (accIs ++ mis, id') is
+
+lowerInsertInstrSubRegs stf tid2rc (accIs, id) (mi @
+  MachineSingle {msOperands = [d @ MachineTemp {mtId = did},
+                               _,
+                               s2 @ MachineTemp {}, sr]} : is)
   | isMachineSubregToReg mi =
   let subreg     = toSubRegIndex sr
       (id', mis) =
@@ -74,3 +59,73 @@ lowerInsertInstrSubRegs stf tid2rc (accIs, id) (mi : is) =
   lowerInsertInstrSubRegs stf tid2rc (accIs ++ [mi], id) is
 
 lowerInsertInstrSubRegs _ _ (is, acc) [] = (is, acc)
+
+lowerInsertInstrSubRegs' [CopySubRegIndex] id mi @ MachineSingle {msOperands = [d, _, s2, _]}
+  = let cp = mi {msOpcode = mkMachineVirtualOpc COPY, msOperands = [d, s2]}
+  in (id, [cp])
+lowerInsertInstrSubRegs' [LowSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t  = mkSimpleMachineTemp id
+        hi = mi {msOpcode = mkMachineVirtualOpc HIGH, msOperands = [t, s1]}
+        co = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, s2, t]}
+    in (id + 1, [hi, co])
+lowerInsertInstrSubRegs' [HighSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t  = mkSimpleMachineTemp id
+        lo = mi {msOpcode = mkMachineVirtualOpc LOW, msOperands = [t, s1]}
+        co = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, s2, t]}
+    in (id + 1, [lo, co])
+lowerInsertInstrSubRegs' [LowSubRegIndex, LowSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t     = mkSimpleMachineTemp id
+        t'    = mkSimpleMachineTemp (id + 1)
+        t''   = mkSimpleMachineTemp (id + 2)
+        t'''  = mkSimpleMachineTemp (id + 3)
+        lo    = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t, s1]}
+        hi    = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t', t]}
+        co    = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t'', s2, t']}
+        hi'   = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t''', s1]}
+        co'   = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, t'', t''']}
+    in (id + 4, [lo, hi, co, hi', co'])
+lowerInsertInstrSubRegs' [HighSubRegIndex, LowSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t     = mkSimpleMachineTemp id
+        t'    = mkSimpleMachineTemp (id + 1)
+        t''   = mkSimpleMachineTemp (id + 2)
+        t'''  = mkSimpleMachineTemp (id + 3)
+        lo    = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t, s1]}
+        lo'   = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t', t]}
+        co    = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t'', t', s2]}
+        hi    = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t''', s1]}
+        co'   = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, t'', t''']}
+    in (id + 4, [lo, lo', co, hi, co'])
+lowerInsertInstrSubRegs' [LowSubRegIndex, LowSubRegIndex, LowSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t       = mkSimpleMachineTemp id
+        t'      = mkSimpleMachineTemp (id + 1)
+        t''     = mkSimpleMachineTemp (id + 2)
+        t'''    = mkSimpleMachineTemp (id + 3)
+        t''''   = mkSimpleMachineTemp (id + 4)
+        t'''''  = mkSimpleMachineTemp (id + 5)
+        t'''''' = mkSimpleMachineTemp (id + 6)
+        lo      = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t, s1]}
+        lo'     = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t', t]}
+        hi      = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t'', t']}
+        co      = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t''', s2, t'']}
+        hi'     = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t'''', t]}
+        co'     = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t''''', t''', t'''']}
+        hi''    = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t'''''', s1]}
+        co''    = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, t''''', t'''''']}
+    in (id + 7, [lo, lo', hi, co, hi', co', hi'', co''])
+lowerInsertInstrSubRegs' [HighSubRegIndex, LowSubRegIndex, LowSubRegIndex] id mi @ MachineSingle {msOperands = [d, s1, s2, _]}
+  = let t       = mkSimpleMachineTemp id
+        t'      = mkSimpleMachineTemp (id + 1)
+        t''     = mkSimpleMachineTemp (id + 2)
+        t'''    = mkSimpleMachineTemp (id + 3)
+        t''''   = mkSimpleMachineTemp (id + 4)
+        t'''''  = mkSimpleMachineTemp (id + 5)
+        t'''''' = mkSimpleMachineTemp (id + 6)
+        lo      = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t, s1]}
+        lo'     = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t', t]}
+        lo''    = mi {msOpcode = mkMachineVirtualOpc LOW,     msOperands = [t'', t']}
+        co      = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t''', t'', s2]}
+        hi      = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t'''', t]}
+        co'     = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [t''''', t''', t'''']}
+        hi'     = mi {msOpcode = mkMachineVirtualOpc HIGH,    msOperands = [t'''''', s1]}
+        co''    = mi {msOpcode = mkMachineVirtualOpc COMBINE, msOperands = [d, t''''', t'''''']}
+    in (id + 7, [lo, lo', lo'', co, hi, co', hi', co''])
