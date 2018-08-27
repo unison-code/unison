6f8ac189287fec9d9fdd58e82b407f3ae1b902a2
Roberto Castaneda Lozano <rcas@sics.se>
2018-08-27 16:04:52 +0200

    Take into account temporaries in register class map when computing a new temp id

--- src/unison/src/MachineIR/Util.hs
+++ src/unison/src/MachineIR/Util.hs
@@ -54,7 +54,9 @@ flattenMachineBlock MachineBlock {mbId = id, mbInstructions = is} =
 
 newMachineBlockId mf = maxZ (map mbId (mfBlocks mf)) + 1
 
-newMachineTempId mf = maxZ [maxTempId mi | mi <- flattenMachineFunction mf] + 1
+newMachineTempId mf =
+  maxZ ((M.keys $ registerClassMap mf) ++
+        [maxTempId mi | mi <- flattenMachineFunction mf]) + 1
 maxTempId mi @ MachineSingle {} =
   maxZ [id | MachineTemp {mtId = id} <- msOperands mi]
 maxTempId MachineBundle {mbInstrs = mis} = maxZ (map maxTempId mis)
