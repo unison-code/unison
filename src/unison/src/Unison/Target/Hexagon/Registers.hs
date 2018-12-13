3514081ef87d24731e7510857918b5643b298d7f
Roberto Castaneda Lozano <rcas@sics.se>
2018-12-13 20:45:37 +0100

    Declare dummy Hexagon register as caller-saved

--- src/unison/src/Unison/Target/Hexagon/Registers.hs
+++ src/unison/src/Unison/Target/Hexagon/Registers.hs
@@ -146,7 +146,7 @@ reserved = [R29, R30, R31]
 callerSaved =
     [R0, R1, R2, R3, R4, R5, R6, R7, R8,
      R9, R10, R11, R12, R13, R14, R15, R28,
-     P0, P1, P2, P3]
+     P0, P1, P2, P3, F0]
 
 -- | Registers that are preserved across calls
 -- | see getCalleeSavedRegs() function in HexagonRegisterInfo.cpp
