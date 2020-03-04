879fd184b4c4210aec21418d4a841287dc0cd564
Roberto Castaneda Lozano <roberto.castaneda@ed.ac.uk>
2020-03-04 22:24:50 +0100

    Add Mips test function lists to ease debugging

---
+++ src/unison/test/fast/Mips/size/functions.mk
@@ -0,0 +1,23 @@
+EXP_FUNCTIONS = \
+gcc.alias.get_frame_alias_set \
+gcc.expmed.ceil_log2 \
+gcc.insn-output.output_51 \
+gcc.jump.unsigned_condition \
+gcc.rtlanal.insn_dependent_p_1 \
+gcc.rtlanal.parms_set \
+gcc.xexit.xexit \
+gobmk.board.get_last_player \
+gobmk.interface.init_gnugo \
+gobmk.owl_attackpat.autohelperowl_attackpat68 \
+gobmk.owl_defendpat.autohelperowl_defendpat421 \
+gobmk.owl_vital_apat.autohelperowl_vital_apat34 \
+gobmk.patterns.autohelperpat1088 \
+gobmk.patterns.autohelperpat301 \
+h264ref.memalloc.no_mem_exit \
+h264ref.sei.UpdateRandomAccess \
+h264ref.vlc.symbol2uvlc \
+hmmer.tophits.AllocFancyAli \
+mesa.api.glIndexd \
+mesa.api.glVertex2i \
+sphinx3.glist.glist_tail \
+sphinx3.profile.ptmr_init
---
+++ src/unison/test/fast/Mips/speed/functions.mk
@@ -0,0 +1,18 @@
+EXP_FUNCTIONS = \
+gcc.alias.get_frame_alias_set \
+gcc.expmed.ceil_log2 \
+gcc.jump.unsigned_condition \
+gcc.rtlanal.parms_set \
+gcc.xexit.xexit \
+gobmk.board.get_last_player \
+gobmk.owl_attackpat.autohelperowl_attackpat68 \
+gobmk.owl_defendpat.autohelperowl_defendpat421 \
+gobmk.owl_vital_apat.autohelperowl_vital_apat34 \
+gobmk.patterns.autohelperpat1088 \
+h264ref.sei.UpdateRandomAccess \
+h264ref.vlc.symbol2uvlc \
+hmmer.tophits.AllocFancyAli \
+mesa.api.glIndexd \
+mesa.api.glVertex2i \
+sphinx3.glist.glist_tail \
+sphinx3.profile.ptmr_init
